21b1f85f24bbccb980a1d829b9c663aafb12c722
LDudrey <113869466+LDudrey@users.noreply.github.com>
2023-03-15 00:00:04 -0500

    added query and mutations to typedef file

--- server/schemas/typedefs.js
+++ server/schemas/typedefs.js
@@ -11,7 +11,7 @@ const typeDefs = gql`
 
   type Book {
     _id: ID
-    authors: [String]
+    authors: [{String}]
     description: String!
     bookId: String!
     image: String
@@ -20,11 +20,14 @@ const typeDefs = gql`
   }
 
   type Query {
- 
+    me: User
   }
 
   type Mutation {
-    
+    login(email: String!, password: String!): Auth
+    addUser:(username: String!, email:String!, password: String!): Auth
+    saveBook:(authors:[{String}], description: String!, title:String!, bookId:String!, image:String, link:String): User
+    removeBook:(bookId:String!): User
   }
 `;
 
