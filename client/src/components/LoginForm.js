b9f2dad6fd38e6630c4d876a2d0064c88b228e6d
LDudrey <113869466+LDudrey@users.noreply.github.com>
2023-03-20 22:30:08 -0500

    removed uneccessary files

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -1,8 +1,6 @@
 // see SignupForm.js for comments
 import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
-// TODO: Replace the loginUser() functionality imported 
-// from the API file with the LOGIN_USER mutation functionality.
 import { LOGIN_USER } from '../utils/mutations';
 import { useMutation } from '@apollo/client';
 import Auth from '../utils/auth';
