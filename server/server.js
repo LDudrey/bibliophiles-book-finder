d402ff320e40a61002ae4d5a29e99c75706c6ae1
LDudrey <113869466+LDudrey@users.noreply.github.com>
2023-03-21 00:30:16 -0500

    added path to index.html on server.js to test heroku error issue

--- server/server.js
+++ server/server.js
@@ -6,7 +6,6 @@ const { authMiddleware } = require('./utils/auth');
 const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
 
-// const routes = require('./routes');
 const PORT = process.env.PORT || 3001;
 const app = express();
 const server = new ApolloServer({
@@ -23,10 +22,8 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-// app.use(routes);
-
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client'));
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 
 const startApolloServer = async (typeDefs, resolvers) => {
